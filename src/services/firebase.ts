302fc7872f3fa1eeddef1331a583ea0187ab0fe7
Helio Carlos <hcjr2000@gmail.com>
2021-10-24 11:29:06 -0300

    Test environment variables

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,6 +1,8 @@
 import {initializeApp} from 'firebase/app'
 import {GoogleAuthProvider, signInWithRedirect, getAuth, getRedirectResult, onAuthStateChanged} from 'firebase/auth'
 
+console.log(process.env.FIREBASE_CONFIG)
+
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
